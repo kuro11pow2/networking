2051f7a4615c36d5c6a3100d1273aea60baf5b20
kuro11pow2 <kuro11pow2@gmail.com>
2022-07-28 23:59:14 +0900

    테스트

--- TcpSocket/Tests/UnitTest1.cs
+++ TcpSocket/Tests/UnitTest1.cs
@@ -3,14 +3,23 @@ namespace Tests
     public class UnitTest1
     {
         [Theory]
-        [InlineData(@"0123", @"0123")]
         [InlineData(@"abcxyz", @"abcxyz")]
-        public void Test1(string input, string expected)
+        public async Task Test1(string input, string expected)
         {
+            string address = "127.0.0.1";
             int port = 12345;
             TcpServer ts = new TcpServer(port);
             ts.Start();
-            
+
+            TcpClient tc = new TcpClient(address, port);
+            tc.Cid = 0;
+            await tc.StartAsync();
+
+            await tc.SendAsync(input);
+            string actual = await tc.ReceiveAsync();
+
+            Assert.Equal(actual, $"from: {tc.Cid}, {expected}");
+
             // ts.StartMonitor();
             // ts.Stop();
         }
